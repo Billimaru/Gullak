e7885e5329fbd616d20099b7ec1873dab85cdca5
sudhanshu kumawat <sudhanshu.kumawat@habilelabs.io>
2022-06-10 00:46:01 +0530

    This is the initial commit for our final year project gullak

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+  MONGOURI:
+    "mongodb+srv://gullak:gullak@gullak.c8xjglx.mongodb.net/?retryWrites=true&w=majority",
+  JWT_SECRET: "ashish",
+  SENDGRID_API: process.env.SENDGRID_API,
+  EMAIL: "sudhanshukumawat11@gmail.com",
+};
