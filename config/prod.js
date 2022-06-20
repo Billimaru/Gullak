b2a570ac149b84544b50fbc2523aeb0032589d5e
JackTheMofo <ashishkumawat99@protonmail.com>
2022-06-20 11:09:22 +0530

    Updated new repo

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
   MONGOURI:
-    "mongodb+srv://gullak:gullak@gullak.c8xjglx.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://ashish:ashish098@cluster0.jjgz2u0.mongodb.net/?retryWrites=true&w=majority",
   JWT_SECRET: "ashish",
   SENDGRID_API: process.env.SENDGRID_API,
   EMAIL: "sudhanshukumawat11@gmail.com",
